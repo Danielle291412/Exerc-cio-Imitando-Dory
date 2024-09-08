4a68dd218bc3c7e70ab4f11249faddd2d29ed379
Danielle Gouveia <danielle.vieira9@etec.sp.gov.br>
2024-09-08 17:12:01 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("--- Imitando Dory ---\n");
+
+Console.WriteLine("Digite uma frase...: ");
+string frase = Console.ReadLine()!;
+
+string fraseEmBaleies = frase
+    .Replace("a", "aaa" )
+    .Replace("A", "AAA" )
+    .Replace("e", "eee" )
+    .Replace("E", "EEE" )
+    .Replace("i", "iii" )
+    .Replace("I", "III" )
+    .Replace("o", "ooo" )
+    .Replace("O", "OOO" )
+    .Replace("u", "uuu" )
+    .Replace("U", "UUU" )
+;
+
+Console.WriteLine($"\nEm baleiês:\n\n{fraseEmBaleies}");
